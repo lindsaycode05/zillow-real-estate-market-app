d976e80983ce534a40fd4bf4fb0825ddf6d51618
Dragosh Gheceanu <dragoshcode@gmail.com>
2022-02-22 20:50:07 +0200

    Fetch the real estate data from the api

---
+++ utils/fetchApi.js
@@ -0,0 +1,14 @@
+import axios from 'axios';
+
+export const baseUrl = 'https://bayut.p.rapidapi.com';
+
+export const fetchApi = async (url) => {
+  const { data } = await axios.get(url, {
+    headers: {
+      'x-rapidapi-host': 'bayut.p.rapidapi.com',
+      'x-rapidapi-key': 'bbaeda657cmsh4f61c7e8661435fp162678jsn653d9880a5c6',
+    },
+  });
+
+  return data
+};
