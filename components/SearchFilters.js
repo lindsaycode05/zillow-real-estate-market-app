1b23835f0a3492741202573d78498264d5da608d
Dragosh Gheceanu <dragoshcode@gmail.com>
2022-02-23 14:33:12 +0200

    Create property filtering functionality

--- components/SearchFilters.js
+++ components/SearchFilters.js
@@ -13,10 +13,48 @@ import { useRouter } from 'next/router';
 import { MdCancel } from 'react-icons/md';
 import Image from 'next/image';
 
-const SearchFilters = () => {
-return (
-    <h1>hello filters</h1>
-)
-}
+import { filterData, getFilterValues } from '../utils/filterData';
 
-export default SearchFilters
+const SearchFilters = () => {
+  const [filters, setFilters] = useState(filterData);
+
+  const router = useRouter();
+
+  const searchProperties = (filterValues) => {
+    const path = router.pathName;
+    const { query } = router;
+
+    const values = getFilterValues(filterValues);
+
+    values.forEach((item) => {
+      query[item.name] = item.value;
+    });
+
+    router.push({ pathname: path, query });
+  };
+
+  return (
+    <Flex bg='gray.100' p='4' justifyContent='center' flexWrap='wrap'>
+      {filters.map((filter) => (
+        <Box key={filter.queryName}>
+          <Select
+            onChange={(e) =>
+              searchProperties({ [filter.queryName]: e.target.value })
+            }
+            placeholder={filter.placeholder}
+            w='fit-content'
+            p='2'
+          >
+            {filter?.items?.map((item) => (
+              <option value={item.value} key={item.value}>
+                {item.name}
+              </option>
+            ))}
+          </Select>
+        </Box>
+      ))}
+    </Flex>
+  );
+};
+
+export default SearchFilters;
